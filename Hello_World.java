227740fdb3e39e43bc58e3091836ebc681854b14
alphadre <50775772+alphadre@users.noreply.github.com>
2020-10-29 11:24:09 +0200

    Add files via upload
    This is a Hello_World java program

---
+++ Hello_World.java
@@ -0,0 +1,11 @@
+public class Hello_World {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello World!");
+    }
+    
+}
